57feea21cba0e5bf09eb51408dea00fc49523bd7
megamind <megamind@megaminds-iMac.local>
2018-04-24 19:24:31 +0530

    Intial project setup for blog project using django

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+# Create your views here.
+
+def list_of_post_by_category(request, category_slug):
+	categories = Category.objects.all();
+	posts = Post.objects.filter(status='published')
+	if category_slug:
+		category = get_object_or_404(Category, slug=category_slug)
+		posts = Post.objects.filter(category=category)
+	template = 'blog/category/list_of_post_by_category.html'
+	context = {'categories':categories, 'posts': posts, 'category': category}
+	return render(request, template, context)
+
+def list_of_post(request):
+	posts = Post.objects.filter(status='published')
+	template = 'blog/post/list_of_post.html'
+	context = {'posts': posts}
+	return render(request, template, context)
+
+def post_detail(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+	template = 'blog/post/post_detail.html'
+	context = {'post': post}
+	return render(request, template, context)
