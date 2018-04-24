7aee08fe95f6a8a4e60d660969dd6d6700e66a5f
megamind <megamind@megaminds-iMac.local>
2018-04-24 22:09:03 +0530

    Added the functionality to post comments from the user side.

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.list_of_post, name='list_all_post'),
-	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
-	url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name='list_of_post_by_category')
+	url(r'^$', views.list_of_post, name = 'list_all_post'),
+	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name = 'post_detail'),
+	url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name = 'list_of_post_by_category'),
+	url(r'^(?P<slug>[-\w]+)/comment/$', views.add_comment, name = 'add_comment')
 ]
