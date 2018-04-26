ae2a56e4b3c2467dd784f6372f6e1be496ccaa4d
megamind <megamind@megaminds-iMac.local>
2018-04-26 21:55:36 +0530

    Updated and added the files for backend functionality.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 	url(r'^$', views.list_of_post, name = 'list_all_post'),
 	url(r'^(?P<slug>[-\w]+)/$', views.post_detail, name = 'post_detail'),
 	url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_post_by_category, name = 'list_of_post_by_category'),
-	url(r'^(?P<slug>[-\w]+)/comment/$', views.add_comment, name = 'add_comment')
+	url(r'^(?P<slug>[-\w]+)/comment/$', views.add_comment, name = 'add_comment'),
+	url(r'^backend/post/new/$', views.new_post, name='new_post'),
+	url(r'^backend/post/$', views.list_of_post_backend, name='list_of_post_backend'),
+	url(r'^backend/(?P<slug>[-\w]+)/edit/$', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from .models import Post, Category
 
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 # Create your views here.
 
 def list_of_post_by_category(request, category_slug):
@@ -16,13 +18,25 @@ def list_of_post_by_category(request, category_slug):
 
 def list_of_post(request):
 	posts = Post.objects.filter(status='published')
+	paginator = Paginator(posts, 1)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+
 	template = 'blog/post/list_of_post.html'
-	context = {'posts': posts}
+	context = {'posts': posts, 'page': page}
 	return render(request, template, context)
 
 def post_detail(request, slug):
 	post = get_object_or_404(Post, slug=slug)
-	template = 'blog/post/post_detail.html'
+	if post.status == 'published':
+		template = 'blog/post/post_detail.html'
+	else:
+		template = 'blog/post/post_preview.html'
 	context = {'post': post}
 	return render(request, template, context)
 
@@ -42,3 +56,52 @@ def add_comment(request, slug):
 	template = 'blog/post/add_comment.html'
 	context = {'form': form}
 	return render(request, template, context) 
+
+#########################################
+##############  Backend #################
+#########################################
+
+def new_post(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.save()
+			return redirect('blog:post_detail', slug=post.slug)
+	else:
+		form = PostForm()
+
+	template = 'blog/backend/new_post.html'
+	context = {'form': form}
+	return render(request, template, context)
+
+def list_of_post_backend(request):
+	posts = Post.objects.all()
+	paginator = Paginator(posts, 20)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+
+	template = 'blog/backend/list_of_post_backend.html'
+	context = {'posts': posts, 'page': page}
+	return render(request, template, context)
+
+def edit_post(request, slug):
+	post = get_object_or_404(Post, slug=slug)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=false)
+			post.author = request.user
+			post.save()
+			return redirect('blog:list_of_post_backend')
+	else:
+		form = PostForm(instance=post)
+	context = {'form': form}
+	template = 'blog/backend/new_post.html'
+	return render(request, template, context)
